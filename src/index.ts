e2d2286ee5208534872e12969b92681e61ac725a
keshav1147 <keshav.1147@zenmonk.tech>
2025-01-01 16:33:30 +0530

    Connect with graphql

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import express from "express";
-// import { expressMiddleware } from "@apollo/server/express4";
-// import createApolloGraphqlServer from "./graphql";
-// import UserService from "./services/user";
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServer } from "@apollo/server";
+import dotenv from "dotenv";
+dotenv.config();
 
 async function init() {
   const app = express();
@@ -9,12 +10,32 @@ async function init() {
 
   app.use(express.json());
 
-  app.get("/", (req, res) => {
-    res.json({ message: "Server is up and running" });
+  //Create Apollo GraphQL Server
+  const gqlServer = new ApolloServer({
+    typeDefs: `
+    type Query {
+      hello: String
+      sayHello(name: String): String
+    }
+    `,
+    resolvers: {
+      Query: {
+        hello: () => "Hello GraphQL",
+        sayHello: (_, { name }) => `Hello ${name}`,
+      },
+    },
   });
+
+  //Start Apollo GraphQL Server
+  await gqlServer.start();
+
+  app.get("/", (req, res) => {
+    res.json("Hello GraphQL");
+  });
+
+  app.use("/graphql", expressMiddleware(gqlServer));
 
   app.listen(PORT, () => console.log(`Server started at PORT:${PORT}`));
 }
 
 init();
- 
